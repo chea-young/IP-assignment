8b8efe682eefafbac1fad637ce92fc80f5cf0267
chea-young <lchy0413@gmail.com>
2020-05-17 01:02:59 +0900

    part7

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,12 +3,19 @@ from django.contrib import admin
 from .models import Question, Choice
 
 class ChoiceInline(admin.TabularInline):
+    #TabularInline 좀 더 작게 StackedInline가 좀 더 크게 나온다.
     model = Choice
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date')
-    list_filter = ['pub_date']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
     inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
