f6e885a1fe4775acdb4147245765ed68c5d078c7
chea-young <lchy0413@gmail.com>
2020-05-20 01:09:24 +0900

    article_acheve 링크추가, article_achieve, detail_achieve class로 바꿈

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'news'
+
 urlpatterns = [
+    path('articles/', views.ArticleView.as_view(), name = 'all_article'),
+    path('articles/<int:year>/', views.year_archive, name = 'year'),
+    path('articles/<int:year>/<int:month>/', views.month_archive , name = 'month' ),
+    path('articles/<int:year>/<int:month>/<int:pk>/', views.DetailView.as_view(), name = 'detail'),
+] 
+
+"""urlpatterns =[
     path('articles/', views.article_archive),
-    path('articles/<int:year>/', views.year_archive),
-    path('articles/<int:year>/<int:month>/', views.month_archive),
+    path('articles/<int:year>/', views.YearView.as_view()), name = 'year_archive'),
+    path('articles/<int:year>/<int:month>/', views.MonthView.as_view(), name = 'month_archive' ),
+    path('articles/<int:year>/<int:month>/<int:pk>/', views.DetailView.as_view(), name = 'article_detail'),
     path('articles/<int:year>/<int:month>/<int:pk>/', views.article_detail),
-]
+]"""
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 from .models import Article
 
-def article_archive(request):
-    a_list = Article.objects.order_by('-pub_date')
-    context = {'article_list' : a_list}
-    return render(request, 'news/article_archive.html',context)
+class ArticleView(generic.ListView):
+    model = Article
+    template_name = 'news/article_archive.html'
+
+    def get_queryset(self):
+        return Article.objects.filter(pub_date__lte=timezone.now())
 
 def year_archive(request, year):
     a_list = Article.objects.filter(pub_date__year=year)
@@ -16,7 +22,25 @@ def month_archive(request, year, month):
     context = {'year': year, 'month': month, 'article_list': a_list}
     return render(request, 'news/month_archive.html', context) 
 
-def article_detail(request, year, month, pk):
-    a_list = Article.objects.filter(pub_date__year=year, pub_date__month=month, pk = pk)
-    context = {'year': year, 'month': month, 'article_list': a_list}
-    return render(request, 'news/aritcle_detail.html', context)
+class DetailView(generic.DetailView):
+    model = Article
+    template_name = 'news/aritcle_detail.html'
+
+"""def article_archive(request):
+    a_list = Article.objects.order_by('-pub_date')
+    context = {'article_list' : a_list}
+    return render(request, 'news/article_archive.html',context)"""
+
+
+"""class YearView(generic.ListView):
+    model = Article
+    template_name = 'news/year_archive.html'
+
+class MonthView(generic.ListView):
+    model = Article
+    template_name = 'news/month_archive.html'"""
+
+#def article_detail(request, year, month, pk):
+    #a_list = Article.objects.filter(pub_date__year=year, pub_date__month=month, pk = pk)
+    #context = {'year': year, 'month': month, 'article_list': a_list}
+    #return render(request, 'news/aritcle_detail.html', context) 
