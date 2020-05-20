a31376f4350f1912ab752d88c2acf30c8db08a5b
chea-young <lchy0413@gmail.com>
2020-05-20 21:49:07 +0900

    add admin

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -5,6 +5,8 @@ from . import models
 class ArticleAdmin(admin.ModelAdmin):
     fieldsets = [ (None, {'fields':['headline']}),
     ('Data information', {'fields':['content','pub_date','reporter',]}), ]
+    list_display = ('headline', 'pub_date')
+    search_fields = ['content']
 
 admin.site.register(models.Reporter)
 admin.site.register(models.Article, ArticleAdmin)
