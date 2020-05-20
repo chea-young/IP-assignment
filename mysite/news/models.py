45f6022ebd40a97ab334cf44a3c8f452badba37b
chea-young <lchy0413@gmail.com>
2020-05-20 21:37:23 +0900

    test 추가

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,5 +1,7 @@
 import datetime
 from django.db import models
+from django.utils import timezone
+
 
 class Reporter(models.Model):
     full_name = models.CharField(max_length=70)
@@ -15,3 +17,7 @@ class Article(models.Model):
 
     def __str__(self):
         return self.headline
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- mysite/news/tests.py
+++ mysite/news/tests.py
@@ -1,3 +1,60 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+from .models import Article
+
+def create_article(article_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Article.objects.create(question_text=article_text, pub_date=time)
+
+class ArticleModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_article = Article(pub_date=time)
+        self.assertIs(future_article.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_article = Article(pub_date=time)
+        self.assertIs(old_article.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_article = Article(pub_date=time)
+        self.assertIs(recent_article.was_published_recently(), True)
+
+class DetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_article = create_article(headline='Future Article.', days=5)
+        url = reverse('news:all_article', args=(future_article.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_article(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_article = create_article(headline='Past Article.', days=-5)
+        url = reverse('news:all_article', args=(past_article.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_article.headline)
