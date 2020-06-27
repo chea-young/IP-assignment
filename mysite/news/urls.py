3def45b9120803f0f2579e1274f7828c2e9ca360
chea-young <study.lchy@gmail.com>
2020-06-28 00:58:04 +0900

    final state

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -6,6 +6,7 @@ app_name = 'news'
 urlpatterns = [
     path('articles/', views.ArticleView.as_view(), name = 'all_article'),
     path('articles/<int:year>/', views.year_archive, name = 'year'),
+    #path('articles/<int:year>/', views.YearView.as_view(), name = 'year_archive'),
     path('articles/<int:year>/<int:month>/', views.month_archive , name = 'month' ),
     path('articles/<int:year>/<int:month>/<int:pk>/', views.DetailView.as_view(), name = 'detail'),
 ] 
@@ -14,6 +15,5 @@ urlpatterns = [
     path('articles/', views.article_archive),
     path('articles/<int:year>/', views.YearView.as_view()), name = 'year_archive'),
     path('articles/<int:year>/<int:month>/', views.MonthView.as_view(), name = 'month_archive' ),
-    path('articles/<int:year>/<int:month>/<int:pk>/', views.DetailView.as_view(), name = 'article_detail'),
     path('articles/<int:year>/<int:month>/<int:pk>/', views.article_detail),
 ]"""
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+
 from .models import Article
 
 class ArticleView(generic.ListView):
@@ -16,6 +17,10 @@ def year_archive(request, year):
     a_list = Article.objects.filter(pub_date__year=year)
     context = {'year': year, 'article_list': a_list}
     return render(request, 'news/year_archive.html', context)
+
+"""class YearView(generic.DetailView):
+    model = Article
+    template_name = 'news/year_archive.html'"""
 
 def month_archive(request, year, month):
     a_list = Article.objects.filter(pub_date__year=year, pub_date__month=month)
@@ -32,11 +37,7 @@ class DetailView(generic.DetailView):
     return render(request, 'news/article_archive.html',context)"""
 
 
-"""class YearView(generic.ListView):
-    model = Article
-    template_name = 'news/year_archive.html'
-
-class MonthView(generic.ListView):
+"""class MonthView(generic.ListView):
     model = Article
     template_name = 'news/month_archive.html'"""
 
